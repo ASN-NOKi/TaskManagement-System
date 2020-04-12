873e0686ee299594be0892edb970956eeee38420
Naoki Asano <brujita10n@gmail.com>
2020-04-12 06:28:36 +0000

    task model references_user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  belongs_to :user
+  
+  validates :content, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { minimum: 5 }
+  
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 20200412060013) do
   create_table "tasks", force: :cascade do |t|
     t.string "content"
     t.string "description"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
