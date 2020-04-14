e60aa477649ec54817eec78ad85cc804e3a39fb5
Naoki Asano <brujita10n@gmail.com>
2020-04-14 09:06:48 +0000

    add Task description page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,26 @@
 class TasksController < ApplicationController
+  before_action :set_user, only: [:new, :show, :edit]
+  
+  
   def new
   end
 
   def index
     @tasks = Task.where(user_id: params[:user_id])
     
-    @user = User.find(1)
-    
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+    # @task = Task.find_by
   end
 
   def edit
   end
+  
+  
+  # def set_user_tasks(user)
+  #   @tasks = Task.where(user_id: user.id)
+  # end
+  
 end
