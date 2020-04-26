375f050211626b3a67b54ccb47888d5956fcaeac
Naoki Asano <brujita10n@gmail.com>
2020-04-26 03:44:25 +0000

    add destroy action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,10 @@ class UsersController < ApplicationController
   end
   
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
   end
   
   def tasks_index
