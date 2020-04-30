526091f32eff3cdf50d4006ef56b1d682134e190
Naoki Asano <brujita10n@gmail.com>
2020-04-30 03:09:23 +0000

    db/seeds.rb modify

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,14 @@ User.create!( name: "浅野 直紀",
               password: "password",
               password_confirmation: "password")
 
+Task.create!( content: "参考URL",
+              description: "以下のURL参照
+              URL1: https://qiita.com/satoken0417/items/df4098a122d05d69a3e6
+              URL2: https://chopesu.com/programing/rails-texturl-link/
+              URL3: https://ja.coder.work/so/ruby-on-rails/1506762
+              URL4: https://pikawaka.com/rails/form_with",
+              user_id: 1)
+              
 20.times do |task|
   content = "content-#{task + 1}"
   description = "#{task + 1}-abcde"
@@ -30,6 +38,7 @@ User.create!( name: "浅野 直紀",
                user_id: user_id)
 end
 
+ 
 10.times do |task|
   content = "テスト業務No.#{task + 1}"
   description = "No.#{task + 1}の業務です。"
